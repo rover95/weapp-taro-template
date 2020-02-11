5d5bde8d3dcdd7e3a0f3027f34ef433d0541f2cf
rover95 <rover95rx@gmail.com>
2020-02-11 23:26:08 +0800

    通用action抛出异常

--- src/utils/createAction.js
+++ src/utils/createAction.js
@@ -5,30 +5,32 @@ import getType from '../utils/createActionType';
   构建异步请求的action
   @param cb Function 请求后的回调函数, 在异步数据存入state前进行预处理
 */
-export function createAction({method,url,data,type},cb){
+export function createAction({ method, url, data, type }, cb) {
   return dispatch => {
     const actionType = getType(type);
     dispatch({
       type: type,
     });
-    return request[method](url,data).then(res=>{
-      if (res.statusCode === 200 || res.statusCode === 204){
+    return request[method](url, data).then(res => {
+      if (res.statusCode === 200 || res.statusCode === 204) {
         dispatch({
           type: actionType.success,
-          payload: cb ? cb(res.data) : res.data 
+          payload: cb ? cb(res.data) : res.data
         });
-      }else{
+      } else {
         dispatch({
           type: actionType.fail,
           error: { message: res.data.message || '请求出错' }
         });
+        throw new Error(res);
       }
       return res;
-    }).catch(err=>{
+    }).catch(err => {
       dispatch({
         type: actionType.fail,
-        error: { message: '网络异常'}
+        error: { message: '网络异常' }
       });
+      throw new Error(err);
       return err;
     });
   };
